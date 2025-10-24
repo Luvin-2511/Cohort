faca6667db1393f6b5ee2a40ac1e6c3f2253e971
Luvin <luvdaku2212@gmail.com>
2025-10-24 22:11:38 +0530

    Task-9 Updated

---
+++ Task-9/script.js
@@ -0,0 +1,13 @@
+const scroller = document.querySelector(".scroller");
+const dots = document.querySelectorAll(".dots");
+
+function handleScroll() {
+    const scrollPos = (scroller.scrollLeft) ;
+    const sectionWidth = (scroller.clientWidth)-650;
+    const index = Math.floor(scrollPos / sectionWidth)
+    console.log(index)
+
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add('active')
+}
+scroller.addEventListener("scroll", handleScroll);
