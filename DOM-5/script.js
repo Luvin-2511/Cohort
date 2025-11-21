68cb75ec2e68b1052fa95e08f53369db471d276f
Luvin <luvdaku2212@gmail.com>
2025-11-21 09:14:32 +0530

    Dom-5

--- DOM-5/script.js
+++ DOM-5/script.js
@@ -1,12 +1,16 @@
 let image = document.querySelector('.clicker')
 let like = document.querySelector('.like')
 let liker = document.querySelector('.liker')
+let likbut = document.querySelector('.likbut')
+let filled = document.querySelector('.filled')
 
 let rotator = ['25deg','-25deg','0deg']
 
 image.addEventListener('dblclick',()=>{
     let rot= Math.floor(Math.random()*rotator.length)
     like.style.opacity=1
+    likbut.style.opacity=0
+    filled.style.opacity=100
     setTimeout(() => {
         liker.style.rotate=`${rotator[rot]}`     
         like.style.fontSize='9rem'  
