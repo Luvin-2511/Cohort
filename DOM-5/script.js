a285e7623f252bd715848c4f86273778c069bee9
Luvin <luvdaku2212@gmail.com>
2025-11-21 08:53:08 +0530

    Dom-5

---
+++ DOM-5/script.js
@@ -0,0 +1,30 @@
+let image = document.querySelector('.clicker')
+let like = document.querySelector('.like')
+let liker = document.querySelector('.liker')
+
+let rotator = ['25deg','-25deg','0deg']
+
+image.addEventListener('dblclick',()=>{
+    let rot= Math.floor(Math.random()*rotator.length)
+    like.style.opacity=1
+    setTimeout(() => {
+        liker.style.rotate=`${rotator[rot]}`     
+        like.style.fontSize='9rem'  
+    }, 100);
+    setTimeout(() => {
+        liker.style.rotate='0deg'
+        like.style.fontSize='6rem'
+    }, 500);
+    setTimeout(() => {
+        liker.style.top='-100%'
+        liker.style.rotate='0deg'
+        like.style.fontSize='0rem'
+        like.style.opacity='0'
+    }, 1000);
+    setTimeout(() => {
+        liker.style.top='50%'
+        liker.style.rotate='0deg'
+        like.style.fontSize='0rem'
+        like.style.opacity='0'
+    }, 1200);
+})
