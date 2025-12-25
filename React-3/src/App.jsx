ec0f1972afc1ad754fdf6c4524664d01a5cb3e9b
Luvin <luvdaku2212@gmail.com>
2025-12-25 11:37:25 +0530

    React - 3

---
+++ React-3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is First React code</h1>
+      <h5>This is the first class of sheriyans react lorem*100</h5>
+    </div>
+  )
+}
+
+export default App
