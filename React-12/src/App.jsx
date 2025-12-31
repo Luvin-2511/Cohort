8e057da1de7680574737f1f2101b1e3d7557c760
Luvin <luvdaku2212@gmail.com>
2025-12-31 10:24:55 +0530

    React-12 Update

--- React-12/src/App.jsx
+++ React-12/src/App.jsx
@@ -16,9 +16,9 @@ const App = () => {
       }} className='text-3xl font-bold rounded active:scale-95 cursor-pointer px-4 py-2 bg-emerald-600'>Click Me MF</button>
       <div className='min-h-full bg-black flex-wrap w-full flex gap-5 p-4 m-6 border-2 border-white rounded-2xl'>
         {tasks.map((elem, idx) => {
-          let col1 = Math.floor(Math.random() * 256)
-          let col2 = Math.floor(Math.random() * 256)
-          let col3 = Math.floor(Math.random() * 256)
+          let col1 = Math.floor(Math.random() * 255)
+          let col2 = Math.floor(Math.random() * 255)
+          let col3 = Math.floor(Math.random() * 255)
           return <div key={idx} style={{ backgroundColor: `rgb(${col1},${col2},${col3})` }} className='flex hover:scale-105 transition-all h-fit w-[49%] rounded-2xl p-6 bg-red-500 items-center'>
             <h4 className='text-xl font-bold'>{elem.id}</h4>
             <h1 className={`text-3xl ${elem.completed ? 'line-through' : ''} font-bold`}>{elem.title}</h1>
