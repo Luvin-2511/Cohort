c439b7dd0e52795727160f0bc2ffd18c8d6415fb
Luvin <luvdaku2212@gmail.com>
2026-01-02 09:13:53 +0530

    Todo

--- React_Personal_Todo/src/App.jsx
+++ React_Personal_Todo/src/App.jsx
@@ -1,41 +1,51 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  // Task useStates
   const localdata = JSON.parse(localStorage.getItem('all-tasks')) || []
   const [alltask, setalltask] = useState(localdata)
   const [task, settask] = useState('')
+
+  // Level useStates
+  const [level, setlevel] = useState(1)
+  const [exp, setexp] = useState(0)
+  const [maxExp, setmaxExp] = useState(100)
+
+  // Submitter
   const submitter = (e) => {
     e.preventDefault()
-    let oldtasks =[...alltask]
+    let oldtasks = [...alltask]
     oldtasks.push({ task, isCompleted: false })
     setalltask(oldtasks)
-    localStorage.setItem('all-tasks',JSON.stringify(oldtasks))
+    localStorage.setItem('all-tasks', JSON.stringify(oldtasks))
     settask('')
   }
-  
+
+  // Deleter
   const deleter = (idx) => {
     let copytasks = [...alltask]
     copytasks.splice(idx, 1)
-    localStorage.setItem('all-tasks',JSON.stringify(copytasks))
+    localStorage.setItem('all-tasks', JSON.stringify(copytasks))
     setalltask(copytasks)
   }
-  
+
+  // Editor
   const editor = (idx) => {
     settask(alltask[idx].task)
     deleter(idx)
   }
-  
+
+  // Toggle Complete
   const toggleComplete = (idx) => {
-    const updated = setalltask(alltask.map((e, i) => 
+    const updated = alltask.map((e, i) =>
       i === idx ? { ...e, isCompleted: !e.isCompleted } : e
-  ))
-  setalltask(updated)
-  localStorage.setItem('all-tasks',JSON.stringify(updated))
-    
+    )
+    setalltask(updated)
+    localStorage.setItem('all-tasks', JSON.stringify(updated))
   }
 
   return (
-    <div className='h-screen w-full p-4 bg-black text-white'>
+    <div className='h-screen w-full bg-[#0F0A1F] p-4 text-white'>
       <form onSubmit={(e) => {
         submitter(e)
       }} className='flex pt-6 items-center gap-2 justify-center w-full'>
@@ -51,15 +61,15 @@ const App = () => {
         <button style={{ boxShadow: '0px 4px 12px rgb(0,0,0,0.4)' }} className='bg-[#BE2DFE] px-4 py-[0.8rem] active:scale-95 hover:animate-pulse cursor-pointer rounded-2xl shadow-2xl'>Add Task</button>
       </form>
 
-      <div className='conta h-1/2 lg:w-1/3 flex flex-col m-4 bg-[#161327] rounded-3xl p-3 '>
+      <div className='conta h-1/2 lg:w-[45rem] flex flex-col m-auto mt-8  bg-[#201c2f00] rounded-3xl p-3 '>
         <h1 className='text-5xl font-bold text-center mt-1.5'>TASKS</h1>
         <h4 className='text-center text-gray-400'>{alltask.length} Active • Today</h4>
         <hr className='w-full' />
         {alltask.map((elem, idx) => {
-          return <div  className='relative overflow-hidden insider border-[0.2px] cursor-pointer flex justify-between hover:scale-[1.02] transition-all items-center px-4 py-2 rounded-xl mt-2 border-gray-400'>
+          return <div className='relative overflow-hidden insider border-[0.2px] cursor-pointer flex justify-between hover:scale-[1.02] transition-all items-center px-4 py-2 rounded-xl mt-2 border-gray-400'>
             <div className='relative z-10 flex items-center  gap-4'>
               <input onChange={() => { toggleComplete(idx) }} className='hidden' type="checkbox" checked={elem.isCompleted} name="tracker" id={`tracker-${idx}`} />
-              <label className='h-[2rem] w-[2rem] rounded-full bg-gray-600 flex items-center justify-center' htmlFor={`tracker-${idx}`}>
+              <label className='h-[2rem] w-[2rem] cursor-pointer rounded-full bg-gray-600 flex items-center justify-center' htmlFor={`tracker-${idx}`}>
                 {elem.isCompleted == true ? <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-7 h-7 text-white invert-100 font-bold"
@@ -79,7 +89,7 @@ const App = () => {
             <div className='relative z-10 flex gap-2'>
               <button onClick={() => {
                 editor(idx)
-              }} className='p-2'>Edit</button>
+              }} className='p-2 cursor-pointer hover:scale-[1.04]'>Edit</button>
               <button onClick={() => {
                 deleter(idx)
               }} className='bg-red-600 hover:bg-red-800 cursor-pointer transition-all px-2 py-2 rounded-xl'>Delete</button>
