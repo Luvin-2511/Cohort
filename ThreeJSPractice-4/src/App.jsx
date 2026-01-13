6efc5edb3bbad216202281c7fd72e4c662e5249a
Luvin <luvdaku2212@gmail.com>
2026-01-13 20:26:30 +0530

    ThreeJS-4

--- ThreeJSPractice-4/src/App.jsx
+++ ThreeJSPractice-4/src/App.jsx
@@ -4,22 +4,35 @@ import { PlaneGeometry } from "three"
 import * as THREE from 'three'
 
 const App = () => {
+  
   const ballRef = useRef(null)
-  const velocity = useRef({x:0.02,y:0,z:0.03})
-  useFrame(()=>{
-    
+  const velocity = useRef({ x: 0.02, y: 0, z: 0.03 })
+  useFrame(() => {
+    ballRef.current.position.y += velocity.current.y;
+    ballRef.current.position.x += velocity.current.x;
+    ballRef.current.position.z += velocity.current.z;
+    velocity.current.y -= 0.001
+    if (ballRef.current.position.x >= 10 || ballRef.current.position.x <= -10) {
+      velocity.current.x = -velocity.current.x
+    }
+    if (ballRef.current.position.z >= -5 || ballRef.current.position.z <= -15) {
+      velocity.current.z = -velocity.current.z
+    }
+    if (ballRef.current.position.y <= 0.5) {
+      velocity.current.y = -velocity.current.y * 0.95
+    }
   })
   return (
     <>
       <ambientLight intensity={10} />
       <pointLight args={[10, 10, 10]} />
-      <mesh ref={ballRef} position={[-5, 10, -10]}>
+      <mesh ref={ballRef} position={[0, 7, -10]}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color={0x00FF00} />
       </mesh>
-      <mesh position={[-5,0,-10]} rotation={[-Math.PI/2,-0.005,0]}>
+      <mesh position={[0, 0, -10]} rotation={[-Math.PI / 2, -0.005, 0]}>
         <planeGeometry args={[20, 20]} />
-        <meshStandardMaterial color={0xFF0000} side={THREE.DoubleSide}/>
+        <meshStandardMaterial color={0xFF0000} side={THREE.DoubleSide} />
       </mesh>
     </>
 
