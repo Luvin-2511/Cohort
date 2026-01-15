7f512291c1c6a6780855a9c10eb9487fb5bd502c
Luvin <luvdaku2212@gmail.com>
2026-01-15 19:44:51 +0530

    Solar

--- ThreeJSPractice-6/src/App.jsx
+++ ThreeJSPractice-6/src/App.jsx
@@ -1,12 +1,64 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { SphereGeometry } from 'three'
+
 
 const App = () => {
-  const model = useGLTF('/sun.glb')
-  console.log(model.scene);
+  const sphereGeo = useRef(null)
+  const texture = useTexture('/SunTexture.png')
+  const marsText = useTexture('/mars.jpg')
+  const earText = useTexture('/ear.jpg')
+  const venText = useTexture('/ven.jpg')
+  const merText = useTexture('/mer.jpg')
+  const satText = useTexture('/sat.jpg')
+  const uraText = useTexture('/ura.jpg')
+  const nepText = useTexture('/nep.jpg')
+  const jupText = useTexture('/jup.jpg')
 
+  useFrame(() => {
+    sphereGeo.current.rotation.y += 0.005
+  })
   return (
     <>
-      <primitive object={model.scene} position={[0,0,0]}/>
+      <group>
+        <mesh ref={sphereGeo}>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshStandardMaterial map={texture} emissive='red' emissiveIntensity={2} />
+        </mesh>
+        <mesh position={[0, 3, 0]}>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial map={merText} />
+        </mesh>
+        <mesh position={[2, -2, 0]}>
+          <sphereGeometry args={[0.2, 32, 32]} />
+          <meshStandardMaterial map={venText} />
+        </mesh>
+        <mesh position={[-2, -4, 0]}>
+          <sphereGeometry args={[0.4, 32, 32]} />
+          <meshStandardMaterial map={earText} />
+        </mesh>
+        <mesh position={[-2, 0, 0]}>
+          <sphereGeometry args={[0.4, 32, 32]} />
+          <meshStandardMaterial map={marsText} />
+        </mesh>
+        <mesh position={[4, 3, 0]}>
+          <sphereGeometry args={[0.4, 32, 32]} />
+          <meshStandardMaterial map={satText} />
+        </mesh>
+        <mesh position={[-4, 3, 0]}>
+          <sphereGeometry args={[0.4, 32, 32]} />
+          <meshStandardMaterial map={uraText} />
+        </mesh>
+        <mesh position={[5, 3, 1]}>
+          <sphereGeometry args={[0.4, 32, 32]} />
+          <meshStandardMaterial map={nepText} />
+        </mesh>
+        <mesh position={[5, 3, 1]}>
+          <sphereGeometry args={[0.1, 32, 32]} />
+          <meshStandardMaterial map={jupText} />
+        </mesh>
+      </group>
     </>
   )
 }
--- ThreeJSPractice-6/src/main.jsx
+++ ThreeJSPractice-6/src/main.jsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 createRoot(document.getElementById('root')).render(
   <Canvas>
     <App />
-    <ambientLight intensity={1}/>
-    <pointLight intensity={1}/>
+    <ambientLight intensity={2} />
+    <pointLight intensity={1} />
     <OrbitControls />
+    <EffectComposer>
+      <Bloom intensity={1}
+        luminanceThreshold={0.2}
+        luminanceSmoothing={0.9} />
+    </EffectComposer>
   </Canvas>
-    
 )
