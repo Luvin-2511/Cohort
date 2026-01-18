1d9834bd844695d092eb8f0bd33dde9b71eb74c8
Luvin <luvdaku2212@gmail.com>
2026-01-18 09:15:24 +0530

    UPdate

--- ThreeJSPrctice-7/src/App.jsx
+++ ThreeJSPrctice-7/src/App.jsx
@@ -1,113 +1,17 @@
-import { useAnimations, useGLTF } from '@react-three/drei'
+import { useAnimations, useFBX, useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import React, { useEffect, useRef, useState } from 'react'
 import { DoubleSide } from 'three'
 import * as THREE from 'three'
 import Wall from './components/Wall'
+import { useThree } from '@react-three/fiber'
 
 const App = () => {
-  const [keys, setkeys] = useState({ w: false, s: false, a: false, d: false, shift: false })
+  const [keys, setkeys] = useState({ w: false, s: false, a: false, d: false })
   const playerRef = useRef(null)
-  const model = useGLTF('/Walking.glb')
-  const model2 = useGLTF('/Standinganim.glb')
-  const model3 = useGLTF('/Running.glb')
-  const speed = keys.shift ? 0.03 : 0.01
-
-  const walkAnim = useAnimations(model.animations, model.scene)
-  useEffect(() => {
-    if (walkAnim.actions) {
-      const walkAction = Object.values(walkAnim.actions)[0]
-      walkAction?.play()
-    }
-  }, [walkAnim])
-
-  const standAnim = useAnimations(model2.animations, model2.scene)
-  useEffect(() => {
-    if (standAnim.actions) {
-      const standAction = Object.values(standAnim.actions)[0]
-      standAction?.play()
-    }
-  }, [standAnim])
-
-  const runAnim = useAnimations(model3.animations, model3.scene)
-  useEffect(() => {
-    if (runAnim.actions) {
-      const runAction = Object.values(runAnim.actions)[0]
-      runAction?.play()
-    }
-  }, [runAnim])
-
-
-  useEffect(() => {
-    const handleDown = (e) => {
-      if (e.key === 'w') {
-        setkeys(prev => ({ ...prev, w: true }))
-        playerRef.current.rotation.set(0, Math.PI, 0)
-      }
-      if (e.key === 's') {
-        setkeys(prev => ({ ...prev, s: true }))
-        playerRef.current.rotation.set(0, 0, 0)
-      }
-      if (e.key === 'a') {
-        setkeys(prev => ({ ...prev, a: true }))
-        playerRef.current.rotation.set(0, -Math.PI / 2, 0)
-      }
-      if (e.key === 'd') {
-        setkeys(prev => ({ ...prev, d: true }))
-        playerRef.current.rotation.set(0, Math.PI / 2, 0)
-      }
-      if (e.key === 'Shift') {
-        setkeys(prev => ({ ...prev, shift: true }))
-      }
-
-    }
-
-    const handleUp = (e) => {
-      if (e.key === 'w') {
-        setkeys(prev => ({ ...prev, w: false }))
-      }
-      if (e.key === 'a') {
-        setkeys(prev => ({ ...prev, a: false }))
-      }
-      if (e.key === 's') {
-        setkeys(prev => ({ ...prev, s: false }))
-      }
-      if (e.key === 'd') {
-        setkeys(prev => ({ ...prev, d: false }))
-      }
-      if (e.key === 'Shift') {
-        setkeys(prev => ({ ...prev, shift: false }))
-      }
-    }
-
-
-    window.addEventListener('keydown', handleDown)
-    window.addEventListener('keyup', handleUp)
-
-    return () => {
-      window.removeEventListener('keydown', handleDown)
-      window.removeEventListener('keyup', handleUp)
-    }
-  }, [])
-
   const walking = keys.w || keys.s || keys.a || keys.d
-  const running = keys.shift && walking
-
-  useFrame(() => {
-    if (keys.w == true) {
-      playerRef.current.position.z -= speed
-    }
-    if (keys.s == true) {
-      playerRef.current.position.z += speed
-    }
-    if (keys.a == true) {
-      playerRef.current.position.x -= speed
-    }
-    if (keys.d == true) {
-      playerRef.current.position.x += speed
-    }
-  })
-
+  const speed = 0.02
+  const boundryWall = useRef(null)
   const walls = [
     { position: [0, 1.5, 0], rotation: [0, 0, 0], args: [0.1, 3, 5] },
     { position: [2.5, 1.5, -2.5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 5] },
@@ -133,26 +37,135 @@ const App = () => {
     { position: [5, 1.5, -5], rotation: [0, Math.PI, 0], args: [0.1, 3, 5] },
     { position: [-3.5, 1.5, -5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 13] },
     { position: [-6.2, 1.5, -7.5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 7.5] },
-    { position: [-7.5, 1.5, -7.5], rotation: [0, Math.PI , 0], args: [0.1, 3, 2.5] },
-    { position: [7.5, 1.5, -7.5], rotation: [0, Math.PI/2 , 0], args: [0.1, 3, 2.5] },
-    { position: [5.2, 1.5, -5], rotation: [0, Math.PI/2 , 0], args: [0.1, 3, 2.5] },
-    { position: [7.5, 1.5, -2.5], rotation: [0, Math.PI/2 , 0], args: [0.1, 3, 2.5] },
-    { position: [7.5, 1.5, -7.5], rotation: [0, Math.PI , 0], args: [0.1, 3, 5] },
+    { position: [-7.5, 1.5, -7.5], rotation: [0, Math.PI, 0], args: [0.1, 3, 2.5] },
+    { position: [7.5, 1.5, -7.5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 2.5] },
+    { position: [5.2, 1.5, -5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 2.5] },
+    { position: [7.5, 1.5, -2.5], rotation: [0, Math.PI / 2, 0], args: [0.1, 3, 2.5] },
+    { position: [7.5, 1.5, -7.5], rotation: [0, Math.PI, 0], args: [0.1, 3, 5] },
   ]
+
+  const { camera } = useThree()
+  console.log(camera);
+
+
+
+  const model = useGLTF('/Standing.glb')
+  const model2 = useGLTF('/Walking.glb')
+  const animation1 = useAnimations(model.animations, model.scene)
+  const animation2 = useAnimations(model2.animations, model2.scene)
+
+
+  useEffect(() => {
+    if (!animation1.actions) return
+    animation1.actions["Armature|mixamo.com|Layer0"].play()
+  }, [animation1])
+  useEffect(() => {
+    if (!animation2.actions) return
+    animation2.actions["Armature|mixamo.com|Layer0"].play()
+  }, [animation2])
+
+
+  useEffect(() => {
+    const handleDown = (e) => {
+      if (e.key === 'w') {
+        setkeys(prev => ({ ...prev, w: true }))
+        playerRef.current.rotation.set(0, Math.PI, 0)
+      }
+      if (e.key === 's') {
+        setkeys(prev => ({ ...prev, s: true }))
+        playerRef.current.rotation.set(0, 0, 0)
+      }
+      if (e.key === 'a') {
+        setkeys(prev => ({ ...prev, a: true }))
+        playerRef.current.rotation.set(0, -Math.PI / 2, 0)
+      }
+      if (e.key === 'd') {
+        setkeys(prev => ({ ...prev, d: true }))
+        playerRef.current.rotation.set(0, Math.PI / 2, 0)
+      }
+    }
+
+    const handleUp = (e) => {
+      if (e.key === 'w') {
+        setkeys(prev => ({ ...prev, w: false }))
+      }
+      if (e.key === 'a') {
+        setkeys(prev => ({ ...prev, a: false }))
+      }
+      if (e.key === 's') {
+        setkeys(prev => ({ ...prev, s: false }))
+      }
+      if (e.key === 'd') {
+        setkeys(prev => ({ ...prev, d: false }))
+      }
+    }
+
+
+    window.addEventListener('keydown', handleDown)
+    window.addEventListener('keyup', handleUp)
+
+    return () => {
+      window.removeEventListener('keydown', handleDown)
+      window.removeEventListener('keyup', handleUp)
+    }
+  }, [])
+
+
+
+
+
+
+
+  useFrame(() => {
+    const newPos = playerRef.current.position.clone()
+
+    if (keys.w == true) newPos.z -= speed
+    if (keys.s == true) newPos.z += speed
+    if (keys.a == true) newPos.x -= speed
+    if (keys.d == true) newPos.x += speed
+
+    let canMove = true
+
+    if (newPos.x < -9.5 || newPos.x > 9.5 || newPos.z < -9.5 || newPos.z > 9.5) {
+      canMove = false
+    }
+    const playerSize = 0.5
+    for (let wall of walls) {
+      const [wx,,wz] = wall.position
+      const [width, , depth] = wall.args
+      const rotY = wall.rotation[1]
+
+      const isVertical = Math.abs(Math.abs(rotY)-Math.PI/2)<0.001
+      const wallWidth = isVertical?depth:width
+      const walldepth = isVertical?width:depth
+      const distX = Math.abs(newPos.x - wx)
+      const distZ = Math.abs(newPos.z - wz)
+
+      if(distX < (playerSize + wallWidth)/2 && distZ <(playerSize + walldepth/2)){
+        canMove = false
+      }
+
+      if(canMove){
+        playerRef.current.position.copy(newPos)
+        camera.position.lerp(new THREE.Vector3(newPos.x,newPos.y+5,newPos.z+2),0.1)
+        camera.lookAt(newPos.x,newPos.y,newPos.z)
+      }
+    }
+  })
+
 
 
 
   return (
     <>
       {/* Player */}
-      <group ref={playerRef} >
-        <primitive rotation={[0, 0, 0]} visible={walking && !running} object={model.scene} />
-        <primitive rotation={[0, 0, 0]} visible={!walking} object={model2.scene} />
-        <primitive rotation={[0, 0, 0]} visible={running} object={model3.scene} />
+      <group ref={playerRef} rotation={[0, 0, 0]} position={[-9, 0, 9]}>
+        <primitive visible={!walking} object={model.scene} />
+        <primitive visible={walking} object={model2.scene} />
       </group>
       {/* Walls */}
-      {walls.map((wall) => {
-        return <Wall rotation={wall.rotation} position={wall.position} args={wall.args} />
+      {walls.map((wall, idx) => {
+        return <Wall key={idx} rotation={wall.rotation} position={wall.position} args={wall.args} />
       })}
 
       {/* Corners */}
