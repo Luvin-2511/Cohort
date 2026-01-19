107d8c3d7aaceb7e5aca27c1336aa0931b66eb56
Luvin <luvdaku2212@gmail.com>
2026-01-19 08:58:09 +0530

    Three Js

--- ThreeJSPractice-8/src/App.jsx
+++ ThreeJSPractice-8/src/App.jsx
@@ -1,17 +1,50 @@
-import { useRef } from 'react'
-import {useFrame} from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
+import { DoubleSide } from 'three'
 
 const App = () => {
-  const boxRef = useRef(null)
+  const [click, setClick] = useState(null)
+  const [firework, setFirework] = useState([])
+
+  const handleClick = (e) => {
+    const newFirework = {
+      id: Date.now(),
+      position: [e.point.x, e.point.y, e.point.z],
+      velocity:0.1
+    }
+
+    setFirework([...firework,newFirework])
+  }
+
   useFrame(()=>{
-    boxRef.current.rotation.x += 0.01
-    boxRef.current.rotation.y += 0.01
+    setFirework(prev=>prev.map((fw)=>({
+      ...fw,
+      position:[fw.position[0],fw.position[1]+fw.velocity,fw.position[2]]
+    })))
   })
+
+
   return (
     <>
-      <mesh ref={boxRef}>
-        <boxGeometry args={[1,1,1]}/>
-        <meshStandardMaterial color='red'/>
+      <color attach="background" args={['#000510']} />
+      {/* Sphere */}
+      {firework.map((fw)=>{
+        return <mesh key={fw.id} position={fw.position}>
+          <sphereGeometry args={[0.2, 32, 32]} />
+          <meshStandardMaterial color='red' />
+        </mesh>
+      })
+      }
+      {/* BG */}
+      <mesh onClick={(e) => {
+        handleClick(e)
+      }}
+        visible={false}
+        position={[0, 5, -5]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <planeGeometry args={[30, 30]} />
+        <meshStandardMaterial color='#000510' side={DoubleSide} />
       </mesh>
     </>
   )
