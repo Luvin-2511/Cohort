6a16af95de05301658397bdf4c16da65ae153e84
Luvin <luvdaku2212@gmail.com>
2026-01-26 13:35:08 +0530

    Web-1

--- ThreeJSPractice-14/src/App.jsx
+++ ThreeJSPractice-14/src/App.jsx
@@ -1,10 +1,32 @@
+import { Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
-const App = () => {
+const ThreeElement = ({ scale }) => {
+  const model = useGLTF('/Miles.glb')
   return (
-    <div>
-      
-    </div>
+    <Canvas>
+      <Environment preset="sunset" />
+      <ambientLight intensity={10} position={[0, -2, 0]} />
+      {/* <OrbitControls /> */}
+      <primitive object={model.scene} position={[0, -4, 0]} scale={scale} />
+    </Canvas>
+  )
+}
+
+const App = () => {
+
+  return (
+    <>
+      <main>
+        <div className="abover">
+          <nav>
+            
+          </nav>
+          <ThreeElement scale={4} />
+        </div>
+      </main>
+    </>
   )
 }
 
