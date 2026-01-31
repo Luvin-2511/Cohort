98b56eb910d7a56665269c06b1f12664483b01de
Luvin <luvdaku2212@gmail.com>
2026-01-31 10:55:14 +0530

    Backend-4 Done

--- Backend-4/src/app.js
+++ Backend-4/src/app.js
@@ -5,17 +5,23 @@ app.use(express.json())
 
 const notes = []
 
-app.post('/notes',(req,res)=>{
+app.post('/notes', (req, res) => {
     notes.push(req.body)
     res.send('Notes created !')
 })
 
-app.get('/notes',(req,res)=>{
+app.get('/notes', (req, res) => {
     res.send(notes)
 })
 
-app.patch('/notes/:index',(req,res)=>{
+app.patch('/notes/:index', (req, res) => {
     notes[req.params.index] = req.body
+    res.send('Note Updated')
+})
+
+app.delete('/notes/:index', (req, res) => {
+    delete notes[req.params.index]
+    res.send('Note Deleted')
 })
 
 module.exports = app
