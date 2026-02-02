bfef3634f099207445404109bdfca0e0c240ac02
Luvin <luvdaku2212@gmail.com>
2026-02-02 19:24:18 +0530

    UPdated Backend-5

--- Backend-5/src/app.js
+++ Backend-5/src/app.js
@@ -7,21 +7,33 @@ const notes = []
 
 app.post('/notes', (req, res) => {
     notes.push(req.body)
-    res.send('Notes created !')
+    res.status(201).json({
+        message: "Notes posted successfully !",
+        notes
+    })
 })
 
 app.get('/notes', (req, res) => {
-    res.send(notes)
+    res.status(200).json({
+        message: "Notes fetched successfully !",
+        notes
+    })
 })
 
 app.patch('/notes/:index', (req, res) => {
     notes[req.params.index] = req.body
-    res.send('Note Updated')
+    res.status(200).json({
+        message: "Note update successfully !",
+        notes
+    })
 })
 
 app.delete('/notes/:index', (req, res) => {
     delete notes[req.params.index]
-    res.send('Note Deleted')
+    res.status(204).json({
+        message: "Note deleted successfully !",
+        notes
+    })
 })
 
 module.exports = app
