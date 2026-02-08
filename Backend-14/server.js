47b3bec1f5cb4db4d01816f6d537a4bae549f7ef
Luvin <luvdaku2212@gmail.com>
2026-02-08 10:06:23 +0530

    Remove node_modules from repo

--- Backend-14/server.js
+++ Backend-14/server.js
@@ -1,5 +1,7 @@
 const app = require('./src/app')
 const PORT = 3000
+const connectToDb = require('./config/connection')
+connectToDb()
 
 
 app.listen(PORT,()=>{
--- Backend-14/src/app.js
+++ Backend-14/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+require('dotenv').config()
 
 module.exports = app
