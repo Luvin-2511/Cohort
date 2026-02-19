e8d1cc2b12aee4b7b55b407ed716c060ab23f80c
Luvin <luvdaku2212@gmail.com>
2026-02-19 09:34:29 +0530

    Added post routes

--- Ins/Backend/src/routes/post.routes.js
+++ Ins/Backend/src/routes/post.routes.js
@@ -4,6 +4,8 @@ const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
-postRouter.post("/", upload.single("post"), postController.createPost);
+postRouter.post("/",upload.single('post'), postController.createPost);
+postRouter.get("/", postController.getPost);
+postRouter.get("/details/:postId", postController.getPostDetails);
 
 module.exports = postRouter;
