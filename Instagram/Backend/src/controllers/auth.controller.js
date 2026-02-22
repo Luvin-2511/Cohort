4b65a5ba5e641101f95f0f3db5d465883da967b6
Luvin <luvdaku2212@gmail.com>
2026-02-22 11:18:19 +0530

    Login Page Responsive

--- Instagram/Backend/src/controllers/auth.controller.js
+++ Instagram/Backend/src/controllers/auth.controller.js
@@ -6,174 +6,173 @@ const jwt = require("jsonwebtoken");
  * Register User
  */
 async function registerController(req, res) {
-  const { username, email, password } = req.body;
+    const {username, email, password} = req.body;
 
-  /**
-   * If User doesn't enter something it will return
-   */
-  if (!username || !email || !password) {
-    return res.status(400).json({
-      message: "Please fill all the required fields !",
+    /**
+     * If User doesn't enter something it will return
+     */
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please fill all the required fields !",
+        });
+    }
+
+    /**
+     * Checks if User already exists and if yes it will return
+     */
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [
+            {
+                email: email,
+            },
+            {
+                username: username,
+            },
+        ],
     });
-  }
 
-  /**
-   * Checks if User already exists and if yes it will return
-   */
-  const isUserAlreadyExists = await userModel.findOne({
-    $or: [
-      {
-        email: email,
-      },
-      {
-        username: username,
-      },
-    ],
-  });
+    if (isUserAlreadyExists) {
+        return res.status(409).json({
+            message: `User already exists with the same ${isUserAlreadyExists.username === username ? "Username" : "Email"}`,
+        });
+    }
 
-  if (isUserAlreadyExists) {
-    return res.status(409).json({
-      message: `User already exists with the same ${isUserAlreadyExists.username === username ? "Username" : "Email"}`,
+    /**
+     * Hashes the password using bcrypt for security
+     */
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashPassword,
     });
-  }
 
-  /**
-   * Hashes the password using bcrypt for security
-   */
-  const hashPassword = await bcrypt.hash(password, 10);
-  const user = await userModel.create({
-    username,
-    email,
-    password: hashPassword,
-  });
+    /**
+     * Generates token and send it in the cookie
+     */
+    const token = jwt.sign(
+        {
+            username: username,
+            email: email,
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: "7d"},
+    );
 
-  /**
-   * Generates token and send it in the cookie
-   */
-  const token = jwt.sign(
-    {
-      username: username,
-      email: email,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" },
-  );
+    res.cookie("token", token);
 
-  res.cookie("token", token);
-
-  /**
-   * Successfully registered User
-   */
-  res.status(201).json({
-    message: "User registered sucessfully !",
-    user,
-  });
+    /**
+     * Successfully registered User
+     */
+    res.status(201).json({
+        message: "User registered sucessfully !",
+        user,
+    });
 }
 
 /**
  * Login User
  */
 async function loginController(req, res) {
-  const { username, email, password } = req.body;
+    const {username, email, password} = req.body;
 
-  /**
-   * If User doesn't enter something it will return
-   */
-  if (!password) {
-    return res.status(400).json({
-      message: "Please fill all the field correctly",
+    /**
+     * If User doesn't enter something it will return
+     */
+    if (!password) {
+        return res.status(400).json({
+            message: "Please fill all the field correctly",
+        });
+    }
+
+    /**
+     * Finds user based on either username or email and if it doesnt exist it will return
+     */
+    const user = await userModel.findOne({
+        $or: [
+            {
+                username: username,
+            },
+            {
+                email: email,
+            },
+        ],
     });
-  }
 
-  /**
-   * Finds user based on either username or email and if it doesnt exist it will return
-   */
-  const user = await userModel.findOne({
-    $or: [
-      {
-        username: username,
-      },
-      {
-        email: email,
-      },
-    ],
-  });
+    if (!user) {
+        return res.status(404).json({
+            message: "User not registered !",
+        });
+    }
 
-  if (!user) {
-    return res.status(404).json({
-      message: "User not registered !",
+    /**
+     * Verifies the hashed password and if wrong returns
+     */
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Incorrect Password !",
+        });
+    }
+
+    /**
+     * Creates token and send it in cookie
+     */
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        },
+    );
+
+    res.cookie("token", token);
+
+    /**
+     * Logged in successfully
+     */
+    res.status(200).json({
+        message: "Logged in successfully !",
+        user,
     });
-  }
-
-  /**
-   * Verifies the hashed password and if wrong returns
-   */
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(400).json({
-      message: "Incorrect Password !",
-    });
-  }
-
-  /**
-   * Creates token and send it in cookie
-   */
-  const token = jwt.sign(
-    {
-      id: user._id,
-      username: user.username,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "7d",
-    },
-  );
-
-  res.cookie("token", token);
-
-  /**
-   * Logged in successfully
-   */
-  res.status(200).json({
-    message: "Logged in successfully !",
-    user,
-  });
 }
 
 /**
  * Logout User
  */
 async function logoutController(req, res) {
-  res.clearCookie("token")
-  
-  return res.status(200).json({
-    message:"User logout successfully !"
-  })
-}
+    res.clearCookie("token")
 
+    return res.status(200).json({
+        message: "User logout successfully !"
+    })
+}
 
 /**
  * Get current logged-in user Details
  */
-async function getMeController(req,res) {
-  const userId = req.user.id
-  /**
-   * Finding user from Db using current user ID fetched from token
-   */
-  const user = await userModel.findById(userId)
+async function getMeController(req, res) {
+    const userId = req.user.id
+    /**
+     * Finding user from Db using current user ID fetched from token
+     */
+    const user = await userModel.findById(userId)
 
-  return res.status(200).json({
-    message:"User fetched successfully !",
-    user:{
-      username:user.username,
-      email:user.email,
-    }
-  })
+    return res.status(200).json({
+        message: "User fetched successfully !",
+        user: {
+            username: user.username,
+            email: user.email,
+        }
+    })
 }
 
 module.exports = {
-  registerController,
-  loginController,
-  logoutController,
-  getMeController
+    registerController,
+    loginController,
+    logoutController,
+    getMeController
 };
--- Instagram/Frontend/src/Features/Auth/pages/Login.jsx
+++ Instagram/Frontend/src/Features/Auth/pages/Login.jsx
@@ -1,113 +1,141 @@
-import React, {useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer.jsx";
-import axios from 'axios'
+import axios from "axios";
 
 const Login = () => {
-    const navigate = useNavigate()
-    const [username,setUsername] = useState(null)
-    const [password,setPassword] = useState(null)
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleLoginForm = async (e) => {
         e.preventDefault();
-        axios.post("http://localhost:3000/api/auth/login", {
-            username: username,
-            password: password
-        }).then((res) => {
-            console.log(res.data)
-            if (res.status === 200) navigate('/')
-        })
+        try {
+            const res = await axios.post(
+                "http://localhost:3000/api/auth/login",
+                {
+                    username,
+                    password,
+                },
+                {
+                    withCredentials: true,
+                }
+            );
+
+            if (res.status === 200) navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     return (
-        <div>
-            <div>
-                <div className="bg-[#000000] h-screen w-screen">
-                    <div className="cont flex p-32 pt-36 items-center justify-center">
-                        <div className="left relative left-[3vw] top-[1vw] w-[30vw] h-[60vh]">
-                            <div className="image">
-                                <img src="./src/assets/Landing.png" alt=""/>
-                            </div>
-                        </div>
+        <div className="bg-[#000000] min-h-screen w-full flex flex-col justify-between">
+            <div className="flex flex-col md:flex-row items-center justify-center
+                      md:px-16 lg:px-24
+                      py-10 md:py-20 gap-10">
 
-                        <form
-                            onSubmit={handleLoginForm}
-                            className="right relative left-[2vw] bottom-[2vw] w-[30vw] h-[60vh] flex items-center flex-col"
-                        >
-                            <div className="img ">
-                                <img className="h-[13vw]" src="./src/assets/logo.webp" alt=""/>
-                            </div>
-                            <div className="ids flex flex-col gap-[0.35vw] relative bottom-[2.7vw]">
-                                <input
-                                    onInput={(e)=>{setUsername(e.target.value)}}
-                                    className="placeholder-[#A8A886] text-white bg-[#121212] text-[0.7vw] px-[0.5vw] py-[0.6vw] border-[0.05px] rounded-sm border-gray-600 w-[16vw]"
-                                    name="usernameOrEmail"
-                                    type="text"
-                                    placeholder="Username or email"
-                                />
-                                <input
-                                    onInput={(e)=>{setPassword(e.target.value)}}
-                                    className="placeholder-[#A8A886] text-white bg-[#121212] text-[0.7vw] px-[0.5vw] py-[0.6vw] border-[0.05px] rounded-sm border-gray-600 w-[16vw]"
-                                    name="password"
-                                    type="password"
-                                    placeholder="Password"
-                                />
-                            </div>
-                            <div className="button relative bottom-[1.9vw]">
-                                <button
-                                    className="bg-[#3441AF] text-sm rounded-md font-bold text-[#AFAFB3] py-[0.4vw] px-[6.8vw]">
-                                    Log in
-                                </button>
-                            </div>
-                            <div
-                                className="or flex items-center justify-center text-white gap-[1vw] relative bottom-[1vw] text-sm ">
-                                <div className="idhar w-[6.5vw] h-[0.05vw] bg-gray-800"></div>
-                                OR
-                                <div className="udhar  w-[6.5vw] h-[0.05vw] bg-gray-800"></div>
-                            </div>
-                            <div
-                                className="facebook text-md text-[#0095F6] cursor-pointer relative top-[1vw] flex items-center justify-center gap-[0.5vw]">
-                                <svg
-                                    aria-label="Log in with Facebook"
-                                    className="x1lliihq x1n2onr6 x173jzuc"
-                                    fill="currentColor"
-                                    height="20"
-                                    role="img"
-                                    viewBox="0 0 16 16"
-                                    width="20"
-                                >
-                                    <title>Log in with Facebook</title>
-                                    <g clipPath="url(#a)">
-                                        <path
-                                            d="M8 0C3.6 0 0 3.6 0 8c0 4 2.9 7.3 6.8 7.9v-5.6h-2V8h2V6.2c0-2 1.2-3.1 3-3.1.9 0 1.8.2 1.8.2v2h-1c-1 0-1.3.6-1.3 1.3V8h2.2l-.4 2.3H9.2v5.6C13.1 15.3 16 12 16 8c0-4.4-3.6-8-8-8Z"
-                                            fill="currentColor"
-                                        ></path>
-                                    </g>
-                                    <defs>
-                                        <clipPath id="a">
-                                            <rect fill="currentColor" height="16" width="16"></rect>
-                                        </clipPath>
-                                    </defs>
-                                </svg>
-                                <div className="text-sm font-semibold">
-                                    Log in with Facebook
-                                </div>
-                            </div>
-                            <div
-                                className="forgot cursor-pointer hover:underline text-white text-[0.85vw] relative top-[2vw] font-semibold">
-                                Forgot Password?
-                            </div>
-                            <div className="account text-sm relative top-[5vw] font-medium text-white">
-                                Don't have an account?{" "}
-                                <Link to="/register" className="text-blue-400">
-                                    Sign up
-                                </Link>
-                            </div>
-                        </form>
-                    </div>
-                    <Footer/>
+                {/* LEFT IMAGE - hidden on phone */}
+                <div className="hidden md:flex md:w-1/2 justify-center items-center">
+                    <img
+                        className="md:w-[28vw] lg:w-[24vw]"
+                        src="./src/assets/Landing.png"
+                        alt=""
+                    />
                 </div>
+
+                {/* RIGHT FORM */}
+                <form
+                    onSubmit={handleLoginForm}
+                    className="w-full md:w-1/2 flex flex-col items-center"
+                >
+                    {/* LOGO */}
+                    <div className="flex items-center justify-center mb-6">
+                        <img
+                            className="h-[15rem] md:h-[10vw] lg:h-[8vw]"
+                            src="./src/assets/logo.webp"
+                            alt=""
+                        />
+                    </div>
+
+                    {/* INPUT FIELDS */}
+                    <div className="flex flex-col gap-4 w-[90vw] md:w-[20vw] lg:w-[20vw]">
+                        <input
+                            onChange={(e) => setUsername(e.target.value)}
+                            className="rounded-xl text-white bg-[#121212]
+                         placeholder-[#A8A886]
+                         border border-gray-600
+                         px-4 py-3
+                         lg:rounded
+                         lg:text-sm
+                         text-sm md:text-base"
+                            type="text"
+                            placeholder="Username or email"
+                        />
+
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="rounded-xl text-white bg-[#121212]
+                         placeholder-[#A8A886]
+                         border border-gray-600
+                         px-4 py-3
+                         lg:rounded
+                         lg:text-sm
+                         text-sm md:text-base"
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </div>
+
+                    {/* LOGIN BUTTON */}
+                    <button
+                        type="submit"
+                        className="bg-[#3441AF] mt-5
+                       text-[#AFAFB3]
+                       rounded-md font-bold
+                       w-[90vw] md:w-[20vw] lg:w-[20vw]
+                       py-3 md:py-2"
+                    >
+                        Log in
+                    </button>
+
+                    {/* OR DIVIDER */}
+                    <div className="flex items-center w-[90vw] md:w-[20vw] lg:w-[20vw]
+                          justify-center text-white gap-3 my-5 text-sm">
+                        <div className="flex-1 h-[1px] bg-gray-800"></div>
+                        OR
+                        <div className="flex-1 h-[1px] bg-gray-800"></div>
+                    </div>
+
+                    {/* FACEBOOK LOGIN */}
+                    <div className="text-[#0095F6] cursor-pointer flex items-center
+                          justify-center gap-3 font-semibold text-base md:text-sm">
+                        <svg
+                            fill="currentColor"
+                            height="20"
+                            viewBox="0 0 16 16"
+                            width="20"
+                        >
+                            <path d="M8 0C3.6 0 0 3.6 0 8c0 4 2.9 7.3 6.8 7.9v-5.6h-2V8h2V6.2c0-2 1.2-3.1 3-3.1.9 0 1.8.2 1.8.2v2h-1c-1 0-1.3.6-1.3 1.3V8h2.2l-.4 2.3H9.2v5.6C13.1 15.3 16 12 16 8c0-4.4-3.6-8-8-8Z" />
+                        </svg>
+                        <span>Log in with Facebook</span>
+                    </div>
+
+                    {/* FORGOT PASSWORD */}
+                    <div className="text-gray-400 text-sm mt-4 hover:underline cursor-pointer">
+                        Forgot Password?
+                    </div>
+
+                    {/* SIGN UP */}
+                    <div className="text-white text-sm mt-4">
+                        Don't have an account?{" "}
+                        <Link to="/register" className="text-blue-400">
+                            Sign up
+                        </Link>
+                    </div>
+                </form>
             </div>
+
+            <Footer />
         </div>
     );
 };
