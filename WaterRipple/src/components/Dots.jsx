a8c01eddbd88acc0e4428befb0aa782403d606d4
Luvin <luvdaku2212@gmail.com>
2026-03-10 03:50:30 +0530

    Water Ripple effect complete

--- WaterRipple/src/components/Dots.jsx
+++ WaterRipple/src/components/Dots.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { animate, stagger } from "animejs";
 
-const Dots = () => {
+const Dots = ({ gridHeight = 20, gridWidth = 30 }) => {
+  const dots = [];
+
+  for (let i = 0; i < gridHeight; i++) {
+    for (let j = 0; j < gridWidth; j++) {
+      const index = i * gridWidth + j;
+
+      dots.push(
+        <div
+          className="dot-wrapper"
+          data-index={index}
+          key={index}
+        >
+          <div className="dot"></div>
+        </div>
+      );
+    }
+  }
+
+  const handleClick = (e) => {
+    const wrapper = e.target.closest(".dot-wrapper");
+    if (!wrapper) return;
+
+    const index = Number(wrapper.dataset.index);
+
+    animate(".dot-wrapper", {
+      scale: [
+        { to: 1.25, ease: "outSine", duration: 250 },
+        { to: 1, ease: "inOutQuad", duration: 500 }
+      ],
+      translateY: [
+        { to: -15, ease: "outSine", duration: 250 },
+        { to: 0, ease: "inOutQuad", duration: 500 }
+      ],
+      opacity: [
+        { to: 1, duration: 250 },
+        { to: 0.5, duration: 500 }
+      ],
+      delay: stagger(40, {
+        grid: [gridWidth, gridHeight],
+        from: index
+      })
+    });
+  };
+
   return (
-    <div>
-      
+    <div
+      onClick={handleClick}
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${gridWidth},1fr)`
+      }}
+      className="dots-container"
+    >
+      {dots}
     </div>
-  )
-}
+  );
+};
 
-export default Dots
+export default Dots;
