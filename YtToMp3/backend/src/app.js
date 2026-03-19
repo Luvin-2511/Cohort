76a10dd9963ade9dbad2b6d6811187e699241eef
Luvin <luvdaku2212@gmail.com>
2026-03-19 23:25:16 +0530

    Fixed

--- YtToMp3/backend/src/app.js
+++ YtToMp3/backend/src/app.js
@@ -1,15 +1,38 @@
-const express = require('express')
-const converterRouter = require('./routes/converter.route')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const converterRouter = require('./routes/converter.route');
 
-const app = express()
-app.use(express.json())
+const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://yump3.vercel.app', 
+  'https://yump3-git-main-luvin-2511s-projects.vercel.app' 
+];
+
 app.use(cors({
-  origin: [
-    "https://yump3.vercel.app",
-    "https://yump3-git-main-luvin-2511s-projects.vercel.app"
-  ]
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS'],
 }));
-app.use('/api',converterRouter)
 
-module.exports = app
+app.options('*', cors());
+
+app.use(express.json());
+
+app.use('/api', converterRouter);
+
+app.use((err, req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.status(500).json({ error: err.message });
+});
+
+module.exports = app;
