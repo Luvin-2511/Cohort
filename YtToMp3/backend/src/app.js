39cd90cecfbffbf27a32768deca81e980e508d19
Luvin <luvdaku2212@gmail.com>
2026-03-19 23:20:27 +0530

    Fixed

--- YtToMp3/backend/src/app.js
+++ YtToMp3/backend/src/app.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:'https://yump3.vercel.app',
-    credentials:true,
-}))
+  origin: [
+    "https://yump3.vercel.app",
+    "https://yump3-git-main-luvin-2511s-projects.vercel.app"
+  ]
+}));
 app.use('/api',converterRouter)
 
 module.exports = app
