5f86ea4f46c8572e13eeb9a05bd1edb79114547c
Luvin <luvdaku2212@gmail.com>
2026-03-19 23:37:03 +0530

    Fixed

--- YtToMp3/backend/src/app.js
+++ YtToMp3/backend/src/app.js
@@ -4,10 +4,24 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+
+const allowedOrigins = [
+  'https://yump3-git-main-luvin-2511s-projects.vercel.app',
+  'https://yump3-mmqk5j8wi-luvin-2511s-projects.vercel.app', 
+  'http://localhost:5173', 
+]
+
 app.use(cors({
-    origin:'https://yump3-git-main-luvin-2511s-projects.vercel.app',
-    credentials:true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
 }))
-app.use('/api',converterRouter)
+
+app.use('/api', converterRouter)
 
 module.exports = app
