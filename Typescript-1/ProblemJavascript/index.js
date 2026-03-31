39aa6aa5636c821a446bbb476288222c2036016b
luvin25111-del <luvdaku2212@gmail.com>
2026-03-31 09:53:37 +0530

    Typescript - Problem with js

---
+++ Typescript-1/ProblemJavascript/index.js
@@ -0,0 +1,9 @@
+console.log("4"+5) //Returns 9
+console.log("4"-5) //Returns -1
+console.log(true + true) //Returns 2
+console.log(false + false) //Returns 0
+console.log([5,1]+4) //Returns [5,14]
+console.log([5,1]*5) //Return NaN
+console.log(5 + true) //Returns 6
+console.log({}+[] ) //Returns [object, Object]
+
