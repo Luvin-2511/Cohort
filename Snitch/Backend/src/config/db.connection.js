1ec58d7e906a96b444f79e0f0b42425a999a930f
luvin25111-del <luvdaku2212@gmail.com>
2026-04-15 19:48:46 +0530

    Fixing axios error !

--- Snitch/Backend/src/config/db.connection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-async function connectToDB(){
-   await mongoose.connect(process.env.MONGO_URI)
-   console.log('Server connected successfully !')
-}
-
-export default connectToDB
--- Snitch/Backend/src/middlewares/error.middleware.js
+++
@@ -1,7 +0,0 @@
-export function errorHandler(err, req, res, next) {
-    if(!err) return next();
-    res.status(err.status || 500).json({
-        success: false,
-        message:err.message || 'Internal Server Error'
-    })
-}
