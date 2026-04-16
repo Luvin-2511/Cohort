5f2493ef3ca5d02f280ee2637463eb633169f47c
luvin25111-del <luvdaku2212@gmail.com>
2026-04-16 19:18:04 +0530

    Practising socket io

---
+++ PractisingShit-12/Backend/server.js
@@ -0,0 +1,26 @@
+import app from "./src/app.js";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+});
+
+httpServer.listen(3000, () => {
+  console.log(`Server listening at port 3000`);
+});
---
+++ PractisingShit-12/Frontend/src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const App = () => {
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    
+    socket.on("connect", () => {
+      console.log("Connected");
+    });
+    
+    socket.on("");
+    
+    return () => socket.disconnect();
+  }, []);
+  
+  const colors = [
+  "#99D98C",
+  "#B7E4C7",
+  "#D8F3DC",
+  "#FFADAD",
+  "#FFC6FF",
+  "#E7C6FF",
+  "#CDB4DB",
+  "#BDE0FE",
+  "#A0C4FF",
+  "#9BF6FF",
+  "#CAF0F8",
+  "#F1C0E8",
+  "#FDE2E4",
+  "#FFD6A5",
+  "#FDFFB6",
+  "#CAFFBF",
+  "#E0FBFC",
+  "#F1FAEE",
+  "#E3FAFC",
+  "#E6FCF5",
+  "#FFF3BF",
+  "#FFE5EC",
+  "#FFCCD5",
+  "#C8B6FF",
+  "#D0BFFF"
+];
+
+  const [currentColor ,setCurrentColor] = useState(colors[0])
+
+  return (
+    <>
+      <nav>
+        {colors.map((color)=>{
+          return <div onClick={()=>{
+            setCurrentColor(color)
+          }} className="circle" style={{
+            background:color,
+            '--bgColor':color
+          }}></div>
+        })}
+      </nav>
+      <div style={{ "--color": currentColor }} className="main">Socket .io </div>;
+    </>
+  );
+};
+
+export default App;
