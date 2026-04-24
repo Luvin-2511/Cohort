030a7b2c517c2f1dc007cde1b3ee3741101ea463
luvin25111-del <luvdaku2212@gmail.com>
2026-04-24 19:29:41 +0530

    Added configs for safety

--- Stepzy/Backend/src/config/db.connection.js
+++ Stepzy/Backend/src/config/db.connection.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
+import { CONFIG } from './config'
 
 export async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(CONFIG.MONGO_URI)
         console.log('Connected to DB !')
     }catch(err){
         console.log(err)
