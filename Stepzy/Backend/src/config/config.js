80f1db4064e66f0d5a4b665226d3274e33263fa1
luvin25111-del <luvdaku2212@gmail.com>
2026-04-25 07:53:20 +0530

    Working on backend

--- Stepzy/Backend/src/config/config.js
+++ Stepzy/Backend/src/config/config.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+if(!process.env.MONGO_URI) {
+    throw new Error("Mongo Uri is not present !")
+}
+
 export const CONFIG = {
     MONGO_URI:process.env.MONGO_URI
 }
