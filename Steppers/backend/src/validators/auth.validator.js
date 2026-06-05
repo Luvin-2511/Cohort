873757925ff630bfdc3d31e825eea27bc4e9d1bb
luvin25111-del <luvdaku2212@gmail.com>
2026-06-05 09:58:30 +0530

    Auth validators Added

---
+++ Steppers/backend/src/validators/auth.validator.js
@@ -0,0 +1,47 @@
+import { body, validationResult } from "express-validator";
+
+const validator = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
+
+  next();
+};
+
+export const registerValidator = [
+  body("email")
+    .isEmail()
+    .withMessage("Enter a valid email")
+    .trim(),
+
+  body("name")
+    .isAlpha()
+    .withMessage("Name should contain alphabets only")
+    .trim()
+    .isLength({ min: 3, max: 12 })
+    .withMessage("Name should contain minimum 3 and maximum 12 letters"),
+
+  body("password")
+    .isStrongPassword()
+    .withMessage("Enter a strong password"),
+
+  validator,
+];
+
+export const loginValidator = [
+  body("email")
+    .isEmail()
+    .withMessage("Enter a valid email")
+    .trim(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required"),
+
+  validator,
+];
