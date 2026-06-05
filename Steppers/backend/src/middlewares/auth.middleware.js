a5c0dd2ad3443fed2b1ea1369d3d1ffb37abd89c
luvin25111-del <luvdaku2212@gmail.com>
2026-06-05 09:58:03 +0530

    Auth and error middlewares Added

---
+++ Steppers/backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { CONFIG } from "../config/config.js";
+
+/**
+ * @middleware
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export async function authMiddleware(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return next({
+      status: 403,
+      message: "unauthorized User",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, CONFIG.JWT_SECRET);
+    res.user = decoded
+    next()
+  } catch (err) {
+    console.log(err);
+  }
+}
