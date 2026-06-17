1b8adcf9151f36167de247f53b186e40d7cc3731
luvin25111-del <luvdaku2212@gmail.com>
2026-06-17 09:05:00 +0530

    Added Cart Controllers

--- Steppers/backend/src/controllers/product.controller.js
+++ Steppers/backend/src/controllers/product.controller.js
@@ -10,10 +10,10 @@ import { uploadFile } from "../services/storage.service.js";
  */
 export async function createProductController(req, res, next) {
   try {
-    const { title, description, price, quantity } = req.body;
+    const { title, description, price, stock } = req.body;
     const { id } = req.user;
 
-    if (!title || !description || !price || !quantity) {
+    if (!title || !description || !price || !stock) {
       return next({
         status: 400,
         message: "All fields are required !",
@@ -38,7 +38,7 @@ export async function createProductController(req, res, next) {
       },
       seller: id,
       images,
-      quantity
+      stock,
     });
 
     res.status(201).json({
@@ -60,7 +60,7 @@ export async function createProductController(req, res, next) {
 export async function getProductController(req, res, next) {
   try {
     const { id } = req.user;
-    const products = await productModel.find({ seller: id })
+    const products = await productModel.find({ seller: id });
     if (!products) {
       return next({
         status: 404,
@@ -120,19 +120,119 @@ export async function fetchProductDetailController(req, res, next) {
         message: "Product Id missing",
       });
     }
-    const productDetail = await productModel.findOne({ _id: productId }).populate('seller')
+    const productDetail = await productModel
+      .findOne({ _id: productId })
+      .populate("seller");
     if (!productDetail) {
       return next({
         status: 404,
-        message: "Invalid product Id !"
-      })
+        message: "Invalid product Id !",
+      });
     }
 
     return res.status(200).json({
       success: true,
       message: "Fetched Detail",
       productDetail,
-    })
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ * @route PUT api/product/:product
+ * @description Updates the product (Only seller)
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export async function updateProductController(req, res, next) {
+  try {
+    const { id } = req.user;
+    const { productId } = req.params;
+    const { updatedTitle, updatedDescription, updatedPrice, updatedStock } =
+      req.body;
+
+    const product = await productModel.findOne({
+      seller: id,
+      _id: productId,
+    });
+
+    if (!product) {
+      return next({
+        status: 404,
+        message: "Invalid Product!",
+      });
+    }
+
+    product.title = updatedTitle || product.title;
+    product.description = updatedDescription || product.description;
+    product.price.amount = updatedPrice || product.price.amount;
+    product.stock = updatedStock || product.stock;
+
+    await product.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Product Updated successfully",
+      product,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ * @route POST api/product/:productId/variant
+ * @description Adds variant to a specific Product
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export async function addVariantController(req, res, next) {
+  try {
+    const { productId } = req.params;
+    const { id } = req.user;
+
+    const product = await productModel.findOne({
+      seller: id,
+      _id: productId,
+    });
+
+    if (!product) {
+      return next({
+        status: 400,
+        message: "Invalid Product !",
+      });
+    }
+
+    const images = await Promise.all(
+      req.files.map(async (file) => {
+        return await uploadFile({
+          buffer: file.buffer,
+          fileName: file.originalname,
+        });
+      }),
+    );
+
+    const { stock, price, ...attributes } = req.body;
+
+    product.variant.push({
+      images,
+      stock: Number(stock),
+      price: {
+        amount: Number(price),
+        currency: "INR",
+      },
+      attributes,
+    });
+
+    await product.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Variant Added successfully !",
+      product,
+    });
   } catch (err) {
     next(err);
   }
