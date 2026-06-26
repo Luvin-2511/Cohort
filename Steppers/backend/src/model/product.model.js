f1f3921a058ded5e9328bada14fe18537ec87944
luvin25111-del <luvdaku2212@gmail.com>
2026-06-26 09:56:41 +0530

    Updated product model

--- Steppers/backend/src/model/product.model.js
+++ Steppers/backend/src/model/product.model.js
@@ -34,6 +34,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    ratingSum: {
+      type: Number,
+      default:0
+    },
     brand: {
       type: String,
       required: true,
