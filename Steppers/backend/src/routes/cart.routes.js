42e11f48ec8e3e58bf06a6e9c8b3adc733697613
luvin25111-del <luvdaku2212@gmail.com>
2026-06-29 09:58:07 +0530

    Added Cart model

--- Steppers/backend/src/routes/cart.routes.js
+++ Steppers/backend/src/routes/cart.routes.js
@@ -1,8 +1,31 @@
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import {
+  addProductToCartController,
+  deleteProductFromCartController,
+  decreaseCountInCartController,
+  getCartController,
+  increaseCountInCartController,
+} from "../controllers/cart.controller.js";
 
-const cartRouter = Router()
+const cartRouter = Router();
 
-// cartRouter.post('/add/:productId/:variantId',)
+cartRouter.post("/add/:productId", authMiddleware, addProductToCartController);
+cartRouter.get("/get", authMiddleware, getCartController);
+cartRouter.patch(
+  "/:productId",
+  authMiddleware,
+  deleteProductFromCartController,
+);
+cartRouter.patch(
+  "/increase/:productId",
+  authMiddleware,
+  increaseCountInCartController,
+);
+cartRouter.patch(
+  "/decrease/:productId",
+  authMiddleware,
+  decreaseCountInCartController,
+);
 
-
-export default cartRouter
+export default cartRouter;
