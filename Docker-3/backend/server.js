4626ed51d9094c5d9cd2ca3d1eb09a0ddf8e4c95
luvin25111-del <luvdaku2212@gmail.com>
2026-07-07 19:45:21 +0530

    Docker practice backend

---
+++ Docker-3/backend/server.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("Health Check")
+})
+
+app.get('/api/data',(req,res)=>{
+    const movies = [
+  {
+    title: "Inception",
+    director: "Christopher Nolan",
+    year: 2010,
+    genre: "Sci-Fi"
+  },
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    year: 2008,
+    genre: "Action"
+  },
+  {
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    year: 2014,
+    genre: "Sci-Fi"
+  },
+  {
+    title: "Parasite",
+    director: "Bong Joon-ho",
+    year: 2019,
+    genre: "Thriller"
+  },
+  {
+    title: "The Matrix",
+    director: "The Wachowskis",
+    year: 1999,
+    genre: "Sci-Fi"
+  }
+];
+    res.json(movies)
+})
+
+app.listen(3000, () => {
+    console.log("Server running on PORT:3000")
+})
