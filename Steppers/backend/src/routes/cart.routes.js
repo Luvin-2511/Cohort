d7994494622e225e6ad0a54d06bd4867b73d424a
luvin25111-del <luvdaku2212@gmail.com>
2026-07-11 19:22:11 +0530

    Updated Routes in steppers

--- Steppers/backend/src/routes/cart.routes.js
+++ Steppers/backend/src/routes/cart.routes.js
@@ -7,12 +7,18 @@ import {
   getCartController,
   increaseCountInCartController,
   createOrderController,
+  verifyOrderController,
+  orderSuccessDetailController,
+  getAllOrderOfUserController
 } from "../controllers/cart.controller.js";
 
 const cartRouter = Router();
 
 cartRouter.post("/add/:productId", authMiddleware, addProductToCartController);
 cartRouter.post("/payment/create/order", authMiddleware, createOrderController);
+cartRouter.post("/payment/verify/order", authMiddleware, verifyOrderController);
+cartRouter.get("/order-success", authMiddleware, orderSuccessDetailController);
+cartRouter.get("/orders", authMiddleware, getAllOrderOfUserController);
 cartRouter.get("/get", authMiddleware, getCartController);
 cartRouter.patch(
   "/:productId",
