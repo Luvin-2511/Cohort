53081dc4dd69d560c64494f0147fba4bf8f1e448
luvin25111-del <luvdaku2212@gmail.com>
2026-07-13 19:22:23 +0530

    Frontend App updated

--- Steppers/frontend/src/App.jsx
+++ Steppers/frontend/src/App.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRoutes from "./AppRoutes";
+import useAuth from "./features/auth/hooks/useAuth";
+import { useSelector } from "react-redux";
+import InteractiveTags from "./features/shared/components/InteractiveTags";
+import Lenis from "lenis";
+import useProduct from "./features/product/hooks/useProduct";
+import useCart from "./features/cart/hooks/useCart";
+import SuccessToast from "./features/shared/components/ToastContainer";
+import ToastContainer from "./features/shared/components/ToastContainer";
 
 const App = () => {
-  return <AppRoutes />;
+  const { handleMe } = useAuth();
+  const { handleFetchWishlist } = useProduct();
+  const { handleFetchCart } = useCart();
+  const { loading, user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    handleMe();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      handleFetchWishlist();
+      handleFetchCart();
+    }
+  }, [user]);
+
+  if (loading) {
+    return <>Loading</>;
+  }
+
+  return (
+    <>
+      <ToastContainer />
+      <InteractiveTags />
+      <AppRoutes />
+    </>
+  );
 };
 
 export default App;
