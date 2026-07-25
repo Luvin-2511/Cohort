8d17e2bfccdcddfb3faa07d39be456271e027ea2
luvin25111-del <luvdaku2212@gmail.com>
2026-07-25 18:57:21 +0530

    Deleted Od project

--- Stepzy/Backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-import { CONFIG } from './config.js'
-
-async function connectToDB(){
-    await mongoose.connect(CONFIG.MONGO_URI)
-    console.log('Mongo database connected successfully !');
-}
-
-export default connectToDB
